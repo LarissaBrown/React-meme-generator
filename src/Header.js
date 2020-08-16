90bb9a734470d29128580dbc0702d6a5b90bd1e3
Larissa Brown <larisa.brown.johnson@gmail.com>
2020-08-16 15:29:59 -0600

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function Header (){
+    return(
+        <header>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRVWaZNmgxC49sxpnQDaxb6a5k30R6sIw-gBA&usqp=CAU" alt="Problem" />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+export default Header
